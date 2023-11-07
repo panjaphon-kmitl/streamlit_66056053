e4106abdb0951ad65ef954f307976a1cfa58da99
panjaphon <66056053@kmitl.ac.th>
2023-11-07 12:14:08 +0700

    Add data source selection

--- stl_app/05_iris_ml/app.py
+++ stl_app/05_iris_ml/app.py
@@ -8,3 +8,13 @@ st.title('Iris Classifier 🌸')
 st.write("This app uses 4 inputs to predict the variety of iris using "
          "a model built on the Iris dataset. Use the form below"
          " to get started!")
+
+
+# ---- Data source selection ----
+st.header('1. Let\'s input the data for model training')
+data_source_options = [None, 'Data provided by this app', 'My own dataset']
+data_source = st.selectbox(
+    'What data source do you want to use?',
+    options=data_source_options,
+    index=0
+)
